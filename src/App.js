70fca93da0c27ba029a6b473c3ff604328b686af
Hostage15 <liuhaoran8111@gmail.com>
2023-09-04 19:04:05 +1000

    Added in Dynamic Values in Templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "This is the title."
+  const link = "https://www.google.com"
+  const googleTitle = "Google Site"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{ title }</h1>
+      <a href={link}>{ googleTitle }</a>
+      <p>{ Math.random() * 1000 }</p>
     </div>
   );
 }
