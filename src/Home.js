8ae4d73e617c30df9a6473a19e953d0bb532214d
Hostage15 <liuhaoran8111@gmail.com>
2023-09-05 15:26:28 +1000

    Added custom style

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 const Home = () => {
-    const title = "This is the title."
+    const title = "This is the title of home page."
     const link = "https://www.google.com"
     const googleTitle = "Google Site"
 
     return ( 
         <div className="Home">
-            <h1>{ title }</h1>
+            <h3>{ title }</h3>
             <a href={link}>{ googleTitle }</a>
             <p>{ Math.random() * 1000 }</p>
         </div>
