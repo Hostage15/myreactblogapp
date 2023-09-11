ade5d42643702ca7e19b868e21072684d9ed7f4f
Hostage15 <liuhaoran8111@gmail.com>
2023-09-11 17:06:25 +1000

    click event

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,21 @@ const Home = () => {
     const link = "https://www.google.com"
     const googleTitle = "Google Site"
 
+    const handleClick = () => {
+        console.log("CLICKED!!!")
+    }
+
+    const handleClickAgain = (name) => {
+        console.log(name + "!!!!!")
+    }
+
     return ( 
         <div className="Home">
             <h3>{ title }</h3>
             <a href={link}>{ googleTitle }</a>
             <p>{ Math.random() * 1000 }</p>
+            <button onClick={handleClick}>Click here!</button>
+            <button onClick={() => handleClickAgain("What")}>Click Again!</button>
         </div>
      );
 }
